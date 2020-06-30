2b2a0961a180b3a3bad06fe190caa06abb0da159
Gleidys <Gleidys@DESKTOP-A6OIRK2>
2020-06-30 14:11:39 -0400

    First code

---
+++ DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListViewExample
+{
+    public class DataContext
+    {
+        public IList<Person> GetPeople()
+        {
+            return new List<Person>
+            {
+                new Person{ Name = "Peter", PhoneNumber = "+5352675489"},
+                new Person{ Name = "Jhon", PhoneNumber = "+5353423145"},
+                new Person{ Name = "Smith", PhoneNumber = "+5352875637"},
+                new Person{ Name = "Alex", PhoneNumber = "+5354326572"}
+            };
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViewExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataContext context;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            context = new DataContext();
+            
+            MyPeople = context.GetPeople();
+        }
+
+        public IList<Person> MyPeople
+        {
+            get { return (IList<Person>)GetValue(MyPeopleProperty); }
+            set { SetValue(MyPeopleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyPeople.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPeopleProperty =
+            DependencyProperty.Register("MyPeople", typeof(IList<Person>), typeof(MainWindow), new PropertyMetadata(default(IList<Person>)));
+
+
+    }
+}
